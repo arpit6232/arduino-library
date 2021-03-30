2cebc2c451534864342df430fd7400f33ee09043
arpit6232 <arpit.savarkar10@gmail.com>
2021-03-30 12:59:39 -0600

    Updated code

---
+++ examples/person_detection/helper.h
@@ -0,0 +1,37 @@
+
+
+
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+
+
+
+constexpr int kNumCols = 96;
+constexpr int kNumRows = 96;
+constexpr int kNumChannels = 1;
+
+constexpr int kMaxImageSize = kNumCols * kNumRows * kNumChannels;
+
+constexpr int kCategoryCount = 2;
+constexpr int kPersonIndex = 1;
+constexpr int kNotAPersonIndex = 0;
+extern const char* kCategoryLabels[kCategoryCount];
+
+
+// Expose a C friendly interface for main functions.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup();
+
+void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+void RespondToDetection(tflite::ErrorReporter* error_reporter,
+                        int8_t person_score, int8_t no_person_score);
+
