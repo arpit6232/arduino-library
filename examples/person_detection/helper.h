578131f9254f9e570df1be04e845e1c809b8f077
arpit6232 <arpit.savarkar10@gmail.com>
2021-03-30 13:04:31 -0600

    main ino file updateD

--- examples/person_detection/helper.h
+++ examples/person_detection/helper.h
@@ -3,6 +3,11 @@
 
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
 
 
 
--- examples/person_detection/person_detection.ino
+++ examples/person_detection/person_detection.ino
@@ -15,17 +15,9 @@ limitations under the License.
 
 #include <TensorFlowLite.h>
 
-#include "main_functions.h"
-
-#include "detection_responder.h"
+#include "helper.h"
 #include "image_provider.h"
-#include "model_settings.h"
 #include "person_detect_model_data.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
 
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
